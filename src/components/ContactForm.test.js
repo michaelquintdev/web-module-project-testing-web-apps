fea46140425e9accfbc12ec95dc5f09338507e77
michaelquintdev <michaelquintdev@gmail.com>
2021-06-03 22:49:26 -0500

    2nd test throwing errors, 2 other tests are done

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,23 +1,41 @@
 import React from 'react';
-import {render, screen, waitFor } from '@testing-library/react';
+import {queryByPlaceholderText, render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import ContactForm from './ContactForm';
 
 test('renders without errors', ()=>{
-    
+    render(<ContactForm />)
 });
 
 test('renders the contact form header', ()=> {
-    
+    //Arrange
+    render(<ContactForm />)
+    //Act
+    const header = screen.queryByText(/contact form/i)
+    //Assert
+    expect(header).toBeInTheDocument()
 });
 
 test('renders ONE error message if user enters less then 5 characters into firstname.', async () => {
-    
+    //Arrange
+    render(<ContactForm />)
+    //Act and Assert
+    const input = screen.getByLabelText('edd')
+    userEvent.type(input, 'yoo')
+
+    const errorMessages = screen.queryAllByText(/error/i)
+    expect(errorMessages).toHaveLength(1)
 });
 
 test('renders THREE error messages if user enters no values into any fields.', async () => {
-    
+    render(<ContactForm />);
+
+	const button = screen.getByRole('button');
+	userEvent.click(button);
+
+	const errorMessages = screen.queryAllByText(/error/i);
+	expect(errorMessages).toHaveLength(3);
 });
 
 test('renders ONE error message if user enters a valid first name and last name but no email.', async () => {
